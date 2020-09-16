7dcbab4987ca8761d16da470d8439617eee5a740
JoWan1998 <29993999310101@ingenieria.usac.edu.gt>
2020-09-15 21:38:00 -0600

    little changes

--- FRONTEND/JWEditor/src/app/app.module.ts
+++ FRONTEND/JWEditor/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -19,7 +18,6 @@ import { LayoutModule } from '@angular/cdk/layout';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule,
     CodemirrorModule,
